fa310e786a8e648fd01f27fa23e51b6f3a96e3ad
julia <julia.hindel@fau.de>
2021-11-09 17:34:43 -0500

    started on re-scaling

--- Dataloader/dataloader.py
+++ Dataloader/dataloader.py
@@ -1,6 +1,7 @@
 import os
 import nibabel as nib
 import numpy as np
+import cv2
 from torch.utils.data import Dataset
 
 
@@ -22,29 +23,40 @@ class Dataloader(Dataset):
 
     def __getitem__(self, idx):
         if self.data_info["name"] == "MD_PROSTATE":
-            # TODO fix
-            filename = f"prostate_{self.ids[idx]:02}.nii.gz"
+            filename = f"prostate_{self.ids[idx]:02d}.nii.gz"
+        # TODO add specialty other dataset
+
+        # combining path and loading volume
         volume_path = os.path.join(str(self.vol_path), filename)
-        volume = self.read_volume(volume_path)
+
+        volume, resolution = self.read_volume(volume_path)
+
+        # normalization
         volume = minmaxnorm(volume)
-        print(volume.shape)
-        images = slice_volume(volume)
+        volume = re_sampling(volume, resolution, self.data_info["resolution"], self.data_info["dimension"])
 
         if self.seg_path is None:
-            return images
+            return volume
 
         seg_path = os.path.join(str(self.seg_path), filename)
-        seg = self.read_volume(seg_path)
+        seg, resolution = self.read_volume(seg_path)
 
-        return images, seg
+        # normalization
+        seg = minmaxnorm(seg)
+        seg = re_sampling(seg, resolution, self.data_info["resolution"], self.data_info["dimension"])
+
+        return volume, seg
 
     def read_volume(self, path):
         # reads volume and returns
-        volume = nib.load(path)
+        data = nib.load(path)
         # use the T2 image (modility: 0) of this dataset only
-        if self.data_name == "MD_PROSTATE":
-            return volume.get_fdata()[0]
-        return volume.get_fdata()
+        if self.data_info["name"] == "MD_PROSTATE":
+            volume = data.get_fdata()[:, :, :, 0]
+            volume = np.moveaxis(volume, -1, 0)
+            return volume, data.header['pixdim'][1:3]
+        # TODO add specialty other dataset
+        return
 
 
 def minmaxnorm(volume):
@@ -52,6 +64,28 @@ def minmaxnorm(volume):
     max = np.percentile(volume, 99)
     return (volume - min) / (max - volume)
 
-# this is probably just a numpy reshape
-def slice_volume(volume):
-    return volume
+
+def re_sampling(volume, res_old, res_new, dim_new):
+    print(res_old, res_new)
+    re_scale = res_old / res_new
+
+    processed_volume = np.array([])
+
+    for slice in volume:
+        dim = tuple(slice.shape * re_scale)
+        print(slice.shape, dim)
+
+        # resize image -> some error here with numpy
+        resized_slice = cv2.resize(slice, dim, interpolation=cv2.INTER_LINEAR)
+
+        # crop and padding
+        processed_slice = crop_or_pad(resized_slice, dim_new)
+
+        processed_volume.append(processed_slice)
+
+    return processed_volume
+
+
+def crop_or_pad(slice, dim_new):
+    # TODO pad when smaller, crop when bigger
+    return slice
--- Dataloader/test_dataloader.py
+++ Dataloader/test_dataloader.py
@@ -2,8 +2,8 @@ from Dataloader.dataloader import Dataloader
 from Dataloader.init_data import md_prostate
 import numpy as np
 
-test_loader = Dataloader(md_prostate, np.asarray(["00", "01"]), "../Task05_Prostate/images", seg_path="../Task05_Prostate/labels")
+test_loader = Dataloader(md_prostate, np.asarray([0, 1]), "../Task05_Prostate/images", seg_path="../Task05_Prostate/labels")
 
 for test_images, test_labels in test_loader:
-    sample_image = test_images[0]    # Reshape them according to your needs.
+    sample_image = test_images[0]
     sample_label = test_labels[0]
