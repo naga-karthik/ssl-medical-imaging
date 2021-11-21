db078d0e15bdf65e4bf78965766796b13afeb0d3
Naga Karthik <emvnagakarthik@gmail.com>
2021-11-21 16:41:33 -0500

    changed num filters and fc units inputs

--- seg_models.py
+++ seg_models.py
@@ -23,44 +23,47 @@ class SegUnetEncoder_and_ProjectorG1(nn.Module):
     """
     Encoder of the UNet model and the Projector MLP on top of it
     """
-    def __init__(self, in_channels=1, base_n_filter=4, fc_units=1024, g1_out_dim=128):
+    def __init__(self, in_channels=1, num_filters=[1, 16, 32, 64, 128, 128], fc_units=[3200, 1024], g1_out_dim=128):
         super(SegUnetEncoder_and_ProjectorG1, self).__init__()
         self.in_channels = in_channels
-        self.base_n_filter = base_n_filter
+        self.num_filters = num_filters
+
         
         self.upsample = nn.Upsample(scale_factor=2, mode="nearest")
         self.maxpool2d = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.projector_g1 = nn.Sequential(
-            nn.Linear(base_n_filter*32*(3*3), fc_units),    # back of a napkin math gives final enc6 output as 3x3 with base_n_filtersx32 filters
+            nn.Linear(num_filters[5]*(3*3), fc_units[0]),    # back of a napkin math gives final enc6 output as 3x3 with base_n_filtersx32 filters
             nn.ReLU(inplace=True),
-            nn.Linear(fc_units, g1_out_dim)
+            nn.Linear(fc_units[0], fc_units[1]),
+            nn.ReLU(inplace=True),
+            nn.Linear(fc_units[1], g1_out_dim)
         )
 
         # Level 1 context pathway
-        self.conv2d_c1_1 = conv_bnorm_relu(in_channels, base_n_filter)
-        self.conv2d_c1_2 = conv_bnorm_relu(base_n_filter, base_n_filter)
+        self.conv2d_c1_1 = conv_bnorm_relu(in_channels, num_filters[0])
+        self.conv2d_c1_2 = conv_bnorm_relu(num_filters[0], num_filters[0])
 
         # Level 2 context pathway
-        self.conv2d_c2_1 = conv_bnorm_relu(base_n_filter, base_n_filter*2)
-        self.conv2d_c2_2 = conv_bnorm_relu(base_n_filter*2, base_n_filter*2)
+        self.conv2d_c2_1 = conv_bnorm_relu(num_filters[0], num_filters[1])
+        self.conv2d_c2_2 = conv_bnorm_relu(num_filters[1], num_filters[1])
 
         # Level 3 context pathway
-        self.conv2d_c3_1 = conv_bnorm_relu(base_n_filter*2, base_n_filter*4)
-        self.conv2d_c3_2 = conv_bnorm_relu(base_n_filter*4, base_n_filter*4)
+        self.conv2d_c3_1 = conv_bnorm_relu(num_filters[1], num_filters[2])
+        self.conv2d_c3_2 = conv_bnorm_relu(num_filters[2], num_filters[2])
 
         # Level 4 context pathway
-        self.conv2d_c4_1 = conv_bnorm_relu(base_n_filter*4, base_n_filter*8)
-        self.conv2d_c4_2 = conv_bnorm_relu(base_n_filter*8, base_n_filter*8)
+        self.conv2d_c4_1 = conv_bnorm_relu(num_filters[2], num_filters[3])
+        self.conv2d_c4_2 = conv_bnorm_relu(num_filters[3], num_filters[3])
 
         # Level 5 context pathway
-        self.conv2d_c5_1 = conv_bnorm_relu(base_n_filter*8, base_n_filter*16)
-        self.conv2d_c5_2 = conv_bnorm_relu(base_n_filter*16, base_n_filter*16)
+        self.conv2d_c5_1 = conv_bnorm_relu(num_filters[3], num_filters[4])
+        self.conv2d_c5_2 = conv_bnorm_relu(num_filters[4], num_filters[4])
 
         # Level 6 context pathway, level 0 localization pathway
-        self.conv2d_c6_1 = conv_bnorm_relu(base_n_filter*16, base_n_filter*32)
-        self.conv2d_c6_2 = conv_bnorm_relu(base_n_filter*32, base_n_filter*32)
-        self.upsample_conv2d_block_l0 = upsample_conv_bnorm_relu(base_n_filter*32, base_n_filter*16)
+        self.conv2d_c6_1 = conv_bnorm_relu(num_filters[4], num_filters[5])
+        self.conv2d_c6_2 = conv_bnorm_relu(num_filters[5], num_filters[5])
+        self.upsample_conv2d_block_l0 = upsample_conv_bnorm_relu(num_filters[5], num_filters[4])
 
     def forward(self, x):
         
@@ -138,7 +141,11 @@ class SegUnetEncoder_and_ProjectorG1(nn.Module):
 # testing with random inputs
 if __name__ == "__main__":
     input_img = torch.randn(4, 1, 192, 192)    # batch_size x channels x H x W
-    model = SegUnetEncoder_and_ProjectorG1(in_channels=1, base_n_filter=4, g1_out_dim=128)
+    
+    num_filters = [1, 16, 32, 64, 128, 128] 
+    fc_units = [3200, 1024]
+    
+    model = SegUnetEncoder_and_ProjectorG1(in_channels=1, g1_out_dim=128, num_filters=num_filters, fc_units=fc_units)
     encoder_out, proj_out, out_dec, context_feats = model(input_img)
     print(f"encoder output shape: {encoder_out.shape}")
     print(f"projector output shape: {proj_out.shape}")
