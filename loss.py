5413193c23fa26eefc5cb1541970e5e478f67689
GwenLegate <gwendolyne180@gmail.com>
2021-11-23 12:25:59 -0500

    complete implementation of global loss with encoder strategies gr and gd-.  Need to be tested when we have augmentations

--- loss.py
+++ loss.py
@@ -29,77 +29,87 @@ class Loss:
         vect2_norm = torch.transpose(f.normalize(vect2, dim=-1, p=2), -1, -2)
         return torch.matmul(vect1_norm, vect2_norm)
 
-    def create_pos_set(self, mini_batch):
-        # based on nx1x192x192 images per batch: we have n images (n=vol*partition), r image width, c image height
-        # assuming with augmentations minibatch dimensions will be 3nx1x192x192
-        # image 3*n-2 is unaugmented, 3*n-1 is augmentation 1 and 3*n is augmentation 3*n
-        n, l, r, c = mini_batch.shape
+    def create_pos_set(self, latent_mini_batch):
+        # based on being passes output of g1(e())
+        # image 3*n-3 is unaugmented, 3*n-2 is augmentation 1 and 3*n is augmentation 3*n-1
+        # array augmentation_type contains on entry per sample specifying the augmentation type: 0=none, 1=type1, 2 = type2
+        n, c = latent_mini_batch.shape
 
         if self.loss_type == 1:
-            # dimensions: num_pos_combosxindividual_laternt_in_each_comboxlatent_dim
             if self.encoder_strategy == 1:
-                pos = torch.zeros(n/3, 2, 128)
+                pos = torch.zeros(int(n/3), 2, c)
+                augmentation_type = torch.zeros(int(n/3), 2)
             if self.encoder_strategy == 2:
-                pos = torch.zeros(n, 2, 128)
+                pos = torch.zeros(n, 2, c)
+                augmentation_type = torch.zeros(n, 2)
             if self.encoder_strategy == 3:
-                pos = torch.zeros((n/3)*4, 2, 128)
+                pos = torch.zeros((int(n/3)*4), 2, c)
+                augmentation_type = torch.zeros(int((n/3)*4), 2)
 
-            model = seg_models.SegUnetEncoder_and_ProjectorG1()
-            latents = model(mini_batch)  # should be nx128 returned
-            for i in range(n/3):
+            for i in range(1, int((n+3)/3)):
                 if self.encoder_strategy == 1:
                     pairs = torch.zeros(1, 2, 128)
-                    pairs[1, 1, :] = latents[(n * 3) + 1, :]
-                    pairs[1, 2, :] = latents[(n * 3) + 2, :]
+                    pairs[1, 1, :] = latent_mini_batch[(n * 3) - 2, :]
+                    pairs[1, 2, :] = latent_mini_batch[(n * 3) - 1, :]
+                    augmentation_type[n, :] = torch.tensor([1., 2.])
                     pos[n, :, :] = pairs
 
                 if self.encoder_strategy == 2:
                     pairs = torch.zeros(3, 2, 128)
-                    pairs[1, 1, :] = latents[(n * 3) + 1, :]
-                    pairs[1, 2, :] = latents[(n * 3) + 2, :]
-                    pairs[2, 1, :] = latents[(n * 3) + 0, :]
-                    pairs[2, 2, :] = latents[(n * 3) + 1, :]
-                    pairs[3, 1, :] = latents[(n * 3) + 0, :]
-                    pairs[3, 2, :] = latents[(n * 3) + 2, :]
+                    pairs[1, 1, :] = latent_mini_batch[(n * 3) - 2, :]
+                    pairs[1, 2, :] = latent_mini_batch[(n * 3) - 1, :]
+                    augmentation_type[n, :] = torch.tensor([1., 2.])
+                    pairs[2, 1, :] = latent_mini_batch[(n * 3) - 3, :]
+                    pairs[2, 2, :] = latent_mini_batch[(n * 3) - 2, :]
+                    augmentation_type[n, :] = torch.tensor([0., 1.])
+                    pairs[3, 1, :] = latent_mini_batch[(n * 3) - 3, :]
+                    pairs[3, 2, :] = latent_mini_batch[(n * 3) - 1, :]
+                    augmentation_type[n, :] = torch.tensor([0., 2.])
+
                     pos[n, :, :] = pairs
                 if self.encoder_strategy == 3:
-                    # TODO: figure out how to get 2nd image for pos pairs
+                    # TODO: figure out how to get 2nd image for pos pairs (need partition info)
                     pairs = torch.zeros(4, 2, 128)
 
         if self.loss_type == 2:
             pass
 
-        return pos
+        return pos, augmentation_type
 
-    def individual_global_loss(self, latent1, latent2, neg_set, pair_order):
-        n, l = neg_set.shape
+    def create_neg_set(self, latent_mini_batch, pos_set_idx, augmentation_type):
+        n, c = latent_mini_batch.shape
+        neg = torch.zeros(int((n / 3) - 1), c)
+
+        for i in range(1, int((n + 3) / 3)):
+            if i == pos_set_idx:
+                pass
+            else:
+                if augmentation_type == 0:
+                    neg[i, :] = latent_mini_batch[(n * 3) - 3, :]
+                if augmentation_type == 1:
+                    neg[i, :] = latent_mini_batch[(n * 3) - 2, :]
+                if augmentation_type == 2:
+                    neg[i, :] = latent_mini_batch[(n * 3) - 1, :]
+        return neg
+
+    def individual_global_loss(self, latent1, latent2, neg_set):
+        n, c = neg_set.shape
         num = torch.exp(self.cos_sim(latent1, latent2) / self.tau)
         denom = 0
         for i in range(n):
-            denom += torch.exp(self.cos_sim(latent1, neg_set[i, pair_order, :]))
+            denom += torch.exp(self.cos_sim(latent1, neg_set[i, :]))
         denom += num
         return -torch.log(torch.div(num, denom))
 
-    def global_loss(self, pos_set):
-        n, _, _ = pos_set.shape
+    def global_loss(self, prediction):
+        n, _, _ = prediction.shape
         loss = 0
-        if self.encoder_strategy == 1:
-            neg_set = torch.zeros((n-1, 2, 128))
-        if self.encoder_strategy == 2:
-            # TODO: need partition membership info to implement
-            pass
-        if self.encoder_strategy == 3:
-            # TODO: need partition membership info to implement
-            pass
-
+        pos_set, augmentations = self.create_pos_set(prediction)
         for i in range(n):
-            neg_set_idx = 0
-            for j in range(n):
-                if j != i:
-                    neg_set[neg_set_idx, :, :] = pos_set[j, :, :]
-
-            l1 = self.individual_global_loss(pos_set[i, 1, :], pos_set[i, 2, :], neg_set, pair_order=0)
-            l2 = self.individual_global_loss(pos_set[i, 2, :], pos_set[i, 1, :], neg_set, pair_order=1)
+            neg_set = self.create_neg_set(prediction, i, augmentations[i, 0])
+            l1 = self.individual_global_loss(pos_set[i, 1, :], pos_set[i, 2, :], neg_set)
+            neg_set = self.create_neg_set(prediction, i, augmentations[i, 1])
+            l2 = self.individual_global_loss(pos_set[i, 2, :], pos_set[i, 1, :], neg_set)
             loss += l1+l2
 
         return loss / n
