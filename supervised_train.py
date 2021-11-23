f2ede350041e8ab7ad1f99b7786bd392bfbace52
Naga Karthik <emvnagakarthik@gmail.com>
2021-11-23 13:40:20 -0500

    added details for dataloader and seg networks

--- supervised_train.py
+++ supervised_train.py
@@ -15,25 +15,41 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
+# dataloaders and segmentation models
+from seg_models import SegUnetFullModel, SegUnetEncoder_and_ProjectorG1, SegUnetDecoder
+from Dataloader.init_data import acdc, md_prostate
+from Dataloader.dataloader import DataloaderRandom
+from Dataloader.experiments_paper import data_init_acdc, data_init_prostate_md
+
 # list of configurables
 cfg = {
         # summary
-        'exp_name': 'Testing!',
+        'exp_name': 'Random-Random Strategy Run 1',
         # dataset
-        'dataset': ...,
+        'dataset': acdc,
+        'num_train_imgs': 'tr52',   # other options - 'tr1', 'tr8'
+        'comb_train_imgs': 'c1',    # other options - 'c1', 'c2', 'cr3', 'cr4', 'cr5'
+        'img_path': '<add-path-the-acdc-dataset-here>',
+        'seg_path': '<same-as-above>',
+        
         # model
         'model': 'Unet',
-        'num_classes': ...,
+        'num_classes': 3,     # 3 or >3? Confirm
+        'in_channels': 1,
+        'num_filters_list': [1, 16, 32, 64, 128, 128],
+        'fc_units_list': [3200, 1024],
+        'g1_out_dim': [128],
         'precision': 32,
+        
         # optimization
         'epochs': 10,
         'batch_size': 32,
+        'num_worker': 4,
         'opt': 'torch.optim.AdamW',
         'opt_params':{
             'lr': 1e-4,
             'weight_decay': 0,
         },
-        'num_worker': 2,
         # scheduler
         'scheduler': 'torch.optim.lr_scheduler.CosineAnnealingWarmRestarts',
         'scheduler_params':{
@@ -50,10 +66,21 @@ class SegModel(pl.LightningModule):
     def __init__(self, cfg):
         super(SegModel, self).__init__()
         self.cfg = cfg
-        self.net = ...
-        self.train_dataset = ...
-        self.valid_dataset = ...
-        self.test_dataset = ...
+        self.net = SegUnetFullModel(
+            in_channels=self.cfg.in_channels, 
+            num_filters_list=self.cfg.num_filters_list,
+            fc_units=self.cfg.fc_units_list,
+            g1_out_dim=self.cfg.g1_out_dim, 
+            num_classes=self.cfg.num_classes
+        )
+
+        self.train_ids_acdc = data_init_acdc.train_data(self.cfg.num_train_imgs, self.cfg.comb_train_imgs)
+        self.val_ids_acdc = data_init_acdc.val_data(self.cfg.num_train_imgs, self.cfg.comb_train_imgs)
+        self.test_ids_acdc = data_init_acdc.test_data()
+
+        self.train_dataset = DataloaderRandom(self.cfg.dataset, self.train_ids_acdc, self.cfg.img_path, preprocessed_data=False, seg_path=self.cfg.seg_path)
+        self.valid_dataset = DataloaderRandom(self.cfg.dataset, self.val_ids_acdc, self.cfg.img_path, preprocessed_data=False, seg_path=self.cfg.seg_path)
+        self.test_dataset = DataloaderRandom(self.cfg.dataset, self.test_ids_acdc, self.cfg.img_path, preprocessed_data=False, seg_path=self.cfg.seg_path)
         
     def forward(self, x):
         return self.net(x)
