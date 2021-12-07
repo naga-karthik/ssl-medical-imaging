f7003372c1ece453efed2554bd4a17c83af99b58
julia <julia.hindel@fau.de>
2021-12-07 13:01:46 -0500

    updated name of custom dataset

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -234,10 +234,7 @@ class DatasetGR(DatasetGeneric):
 
         print("THIS PART IS NOT CORRECTLY IMPLEMENTED AS NOT REQUIRED")
 
-
-# TODO implement data loader GD
-
-class DatasetGDMinus(DatasetGeneric):
+class DatasetGD(DatasetGeneric):
     def __init__(self, data_info, ids, partition, vol_path, preprocessed_data=False, seg_path=None):
         self.pad_frames = 25
         self.padding_list = []
