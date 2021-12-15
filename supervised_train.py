3d1b8e6b513709292aed3dea588d36b913236866
Arash <arash.ashrafnejad@gmail.com>
2021-12-14 20:46:51 -0500

    final version - please use this for finetuning

--- supervised_train.py
+++ supervised_train.py
@@ -5,10 +5,7 @@ import argparse
 from torch._C import device
 
 import numpy as np
-from torch.nn.modules.module import T
 import matplotlib.pyplot as plt
-from torch.utils.data.sampler import WeightedRandomSampler
-timestamp = time.time()
 
 # machine learning packages
 import wandb
@@ -17,7 +14,7 @@ import torch.nn as nn
 import torch.optim as optim
 import pytorch_lightning as pl
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, dataloader
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
 # dataloaders and segmentation models
@@ -27,37 +24,32 @@ from Dataset.dataset import DatasetRandom
 from Dataset.experiments_paper import data_init_acdc, data_init_prostate_md
 from loss import Loss, multiclass_dice_coeff
 
-img_path = "/home/GRAMES.POLYMTL.CA/u114716/ssl_project/datasets/ACDC"
-seg_path = "/home/GRAMES.POLYMTL.CA/u114716/ssl_project/datasets/ACDC"
-
-parser = argparse.ArgumentParser(description="Random-Random Strategy Run 3")
-
+parser = argparse.ArgumentParser(description="Supurvised learning")
 # all the arguments for the dataset, model, and training hyperparameters
-parser.add_argument('--exp_name', default='R-R_Test-NewUNet-withAugs-tr8', type=str, help='Name of the experiment/run')
+parser.add_argument('--exp_name', default='supervised-acdc-tr2-c1', type=str, help='Name of the experiment/run')
+parser.add_argument('--project_name', default='supervised-train-arash', type=str, help='Name of the project in wandb')
 # dataset
-parser.add_argument('-data', '--dataset', default=acdc, help='Specifyg acdc or md_prostate without quotes')
-parser.add_argument('-nti', '--num_train_imgs', default='tr8', type=str, help='Number of training images, options tr1, tr8 or tr52')
-parser.add_argument('-cti', '--comb_train_imgs', default='c1', type=str, help='Combintation of Train imgs., options c1, c2, cr3, cr4, cr5')
-parser.add_argument('--img_path', default=img_path, type=str, help='Absolute path of the training data')
-parser.add_argument('--seg_path', default=seg_path, type=str, help='Same as path of training data')
+parser.add_argument('--dataset', default='ACDC', type=str, help='Specifyg ACDC or MD_PROSTATE')
+parser.add_argument('--num_train_imgs', default='tr8', type=str, help='Number of training images, options tr1, tr2, tr8 or tr52')
+parser.add_argument('--comb_train_imgs', default='c1', type=str, help='Combintation of Train imgs., options c1, c2, c3, c4, c5, c6')
+parser.add_argument('--img_path', type=str, help='Absolute path of the training data')
+parser.add_argument('--seg_path', type=str, help='Same as path of training data')
 # model
-parser.add_argument('-in_ch', '--in_channels', default=1, type=int, help='Number of input channels')
-# parser.add_argument('-num_flt', '--num_filters_list', nargs='+', default=[16, 32, 64, 128, 256, 512], help='List containing no. of filters for Conv Layers')
-# parser.add_argument('-num_flt', '--num_filters_list', nargs='+', default=[1, 16, 32, 64, 128, 128], help='List containing no. of filters for Conv Layers')
-parser.add_argument('-num_flt', '--init_num_filters', type=int, default=32, help='Initial no. of filters for Conv Layers')
-parser.add_argument('-num_fc', '--fc_units_list', nargs='+', default=[3200, 1024], help='List containing no. of units in FC layers')
-parser.add_argument('-g1_dim', '--g1_out_dim', default=128, type=int, help='Output dimension for the projector head')
-parser.add_argument('-nc', '--num_classes', default=4, type=int, help='Number of classes to segment')
-parser.add_argument('-np', '--num_partitions', default=4, type=int, help='No. of partitions per volume')
-parser.add_argument('-st', '--strategy', default='GR', type=str, help='Strategy for pretraining; Options: GR, GD-, GD, GD-alt')
+parser.add_argument('--in_channels', default=1, type=int, help='Number of input channels')
+parser.add_argument('--init_num_filters', type=int, default=32, help='Initial no. of filters for Conv Layers')
+parser.add_argument('--fc_units_list', nargs='+', default=[3200, 1024], help='List containing no. of units in FC layers')
+parser.add_argument('--g1_out_dim', default=128, type=int, help='Output dimension for the projector head')
 # optimization
-parser.add_argument('-p', '--precision', default=32, type=int, help='Precision for training')
-parser.add_argument('-ep', '--epochs', default=100, type=int, help='Number of epochs to train')
-parser.add_argument('-bs', '--batch_size', default=32, type=int, help='Batch size')
-parser.add_argument('-nw', '--num_workers', default=4, type=int, help='Number of worker processes')
-parser.add_argument('-gpus', '--num_gpus', default=1, type=int, help="Number of GPUs to use")
-parser.add_argument('-lr', '--learning_rate', default=1e-3, type=float, help="Learning rate to use")
-parser.add_argument('-wd', '--weight_decay', default=0.0, type=float, help='Default weight decay')
+parser.add_argument('--precision', default=32, type=int, help='Precision for training')
+parser.add_argument('--epochs', default=10000, type=int, help='Number of epochs to train')
+parser.add_argument('--batch_size', default=64, type=int, help='Batch size')
+parser.add_argument('--num_workers', default=4, type=int, help='Number of worker processes')
+parser.add_argument('--num_gpus', default=1, type=int, help="Number of GPUs to use")
+parser.add_argument('--learning_rate', default=1e-3, type=float, help="Learning rate to use")
+parser.add_argument('--weight_decay', default=1e-3, type=float, help='Default weight decay')
+parser.add_argument('--patience', default=100, type=int, help='number of epochs to wait before early stoping')
+parser.add_argument('--enable_progress_bar', default=False, type=bool, help='by default is disabled since it doesnt work in colab')
+parser.add_argument('--checkpoint_path', default='random-init', type=str, help='full path for the checkpoint in case would like to finetune')
 
 cfg = parser.parse_args()
 
@@ -65,96 +57,125 @@ class SegModel(pl.LightningModule):
     def __init__(self, cfg):
         super(SegModel, self).__init__()
         self.cfg = cfg
-        # self.net = SegUnetFullModel(
-        #     in_channels=self.cfg.in_channels, 
-        #     num_filters_list=self.cfg.num_filters_list,
-        #     fc_units=self.cfg.fc_units_list,
-        #     g1_out_dim=self.cfg.g1_out_dim, 
-        #     num_classes=self.cfg.num_classes
-        # )
-        self.net = UNet(n_channels=self.cfg.in_channels, init_filters=self.cfg.init_num_filters, n_classes=self.cfg.num_classes)
+        if cfg.dataset == 'ACDC':
+          data_init = data_init_acdc
+          dataset_cfg = acdc 
+        elif cfg.dataset  == 'MD_PROSTATE':
+          data_init = data_init_prostate_md
+          dataset_cfg = md_prostate
+        else:
+          print('The dataset is not found')
+        self.num_class = dataset_cfg['num_class']
 
-        self.train_ids_acdc = data_init_acdc.train_data(self.cfg.num_train_imgs, self.cfg.comb_train_imgs)
-        self.val_ids_acdc = data_init_acdc.val_data(self.cfg.num_train_imgs, self.cfg.comb_train_imgs)
-        self.test_ids_acdc = data_init_acdc.test_data()
+        self.net = UNet(n_channels=self.cfg.in_channels,
+                        init_filters=self.cfg.init_num_filters,
+                        n_classes=self.num_class)
 
-        self.train_dataset = DatasetRandom(self.cfg.dataset, self.train_ids_acdc, self.cfg.img_path, preprocessed_data=True, seg_path=self.cfg.seg_path, augmentation=True)
-        self.valid_dataset = DatasetRandom(self.cfg.dataset, self.val_ids_acdc, self.cfg.img_path, preprocessed_data=True, seg_path=self.cfg.seg_path, augmentation=True)
-        self.test_dataset = DatasetRandom(self.cfg.dataset, self.test_ids_acdc, self.cfg.img_path, preprocessed_data=True, seg_path=self.cfg.seg_path, augmentation=False)
+        self.train_ids = data_init.train_data(self.cfg.num_train_imgs, self.cfg.comb_train_imgs)
+        self.valid_ids = data_init.val_data(self.cfg.num_train_imgs, self.cfg.comb_train_imgs)
+        self.test_ids = data_init.test_data()
+
+        self.train_dataset = DatasetRandom(dataset_cfg, self.train_ids,
+                                           self.cfg.img_path,
+                                           seg_path=self.cfg.seg_path, augmentation=True)
+        self.valid_dataset = DatasetRandom(dataset_cfg, self.valid_ids,
+                                           self.cfg.img_path,
+                                           seg_path=self.cfg.seg_path, augmentation=False)
+        self.test_dataset = DatasetRandom(dataset_cfg, self.test_ids,
+                                          self.cfg.img_path,
+                                          seg_path=self.cfg.seg_path, augmentation=False)
 
         self.loss = Loss(loss_type=0, encoder_strategy=None, device=self.device)
-        
+        self.loss_visualization_step = 0.1
+        self.best_valid_loss = 1
+        self.best_train_loss = 1
+
+        self.train_losses = []
+        self.valid_losses = []
+        self.test_losses = []
+        self.init_timestamp = time.time()
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size = self.cfg.batch_size, drop_last=False,
+                             shuffle = True,  num_workers=self.cfg.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.valid_dataset, batch_size = 16, drop_last=False,
+                             shuffle = False, num_workers=self.cfg.num_workers)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size = 16, drop_last=False,
+                             shuffle = False, num_workers=self.cfg.num_workers)
+
     def forward(self, x):
         return self.net(x)
 
-    def compute_loss(self, batch):
+    def compute_loss(self, batch, exclude_background=False):
         imgs, gts = batch
         imgs, gts = imgs.float(), gts.long()
         logits, preds = self.net(imgs)
-        # print(torch.unique(gts), gts.shape)
 
-        gts_one_hot = self.loss.one_hot(gts, num_classes=self.cfg.num_classes)  # convert to one-hot for Dice loss
-        loss = self.loss.compute(proj_feat0=None, proj_feat1=None, proj_feat2=None, partition_size=None, prediction=preds, target=gts_one_hot, multiclass=True)
+        gts_one_hot = self.loss.one_hot(gts, num_classes=self.num_class)
+        loss = self.loss.compute(proj_feat0=None, proj_feat1=None, proj_feat2=None,
+                                 partition_size=None, prediction=preds[:, int(exclude_background):],
+                                 target=gts_one_hot[:, int(exclude_background):], multiclass=True)
         return loss, preds, imgs, gts        
     
     def training_step(self, batch, batch_nb):
         loss, preds, imgs, gts = self.compute_loss(batch)
-        self.log('train_loss', loss, on_step=False, on_epoch=True)
+        self.train_losses += [loss.item()] * len(batch)
 
-        if batch_nb == 0: # once per epoch
-            fig = visualize(preds, imgs, gts)
-            wandb.log({"Training Output Visualizations": fig})
+        if loss < self.best_train_loss - self.loss_visualization_step and batch_nb==0:
+          self.best_train_loss = loss.item()
+          fig = visualize(preds, imgs, gts)
+          wandb.log({"Training Output Visualizations": fig})
+          plt.close()
+
         return loss
 
     def validation_step(self, batch, batch_nb):
-        loss, preds, imgs, gts = self.compute_loss(batch)
-        self.log('valid_loss', loss, on_step=False, on_epoch=True)
+        loss, preds, imgs, gts = self.compute_loss(batch, exclude_background=True)
+        self.valid_losses += [loss.item()] * len(batch)
 
-        if batch_nb == 0: # once per epoch
-            fig = visualize(preds, imgs, gts)
-            wandb.log({"Validation Output Visualizations": fig})
-    
+        # qualitative results on wandb when first batch dice improves by 10%
+        if loss < self.best_valid_loss - self.loss_visualization_step and batch_nb==0:
+          self.best_valid_loss = loss.item()
+          fig = visualize(preds, imgs, gts)
+          wandb.log({"Validation Output Visualizations": fig})
+          plt.close()
+            
     def test_step(self, batch, batch_nb):
-        loss, preds, imgs, gts = self.compute_loss(batch)
-        self.log('test_loss', loss, on_step=False, on_epoch=True)
-
-        # dice score (found online)
-        test_gt_one_hot = self.loss.one_hot(gts.long(), num_classes=self.cfg.num_classes)
-        # computing the dice score, ignoring the background ie class 0
-        test_dice_score = multiclass_dice_coeff(
-            (preds[:, 1:, :, :]).to(self.device), 
-            (test_gt_one_hot[:, 1:, :, :]).to(self.device), 
-            reduce_batch_first=False
-        )
-        print(f"\nDICE SCORE ON THE TEST SET: {test_dice_score}")
+        loss, preds, imgs, gts = self.compute_loss(batch, exclude_background=True)
+        self.test_losses += [loss.item()] * len(batch)
 
         # qualitative results on wandb
         fig = visualize(preds, imgs, gts)
         wandb.log({"Test Output Visualizations": fig})
-        test_img, test_gt = batch
-        test_img = test_img.float()
-        
-        return {'test_dice_score' : test_dice_score}
+        plt.close()
+
+    def on_train_epoch_end(self):
+        train_loss = np.mean(self.train_losses)
+        self.log('train_loss', train_loss)
+        self.train_losses = []
+
+    def on_validation_epoch_end(self):
+        valid_loss = np.mean(self.valid_losses)
+        self.log('valid_loss', valid_loss)
+        self.valid_losses = []
+
+    def on_test_epoch_end(self):
+        test_loss = np.mean(self.test_losses)
+        self.log('test_loss', test_loss)
+        self.test_losses = []
+        self.final_timestamp = time.time()
+        self.log('train+test_time', self.final_timestamp - self.init_timestamp)
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(params=self.parameters(), lr=self.cfg.learning_rate, weight_decay=self.cfg.weight_decay)
-        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=40, eta_min=1e-5)
-        
+        optimizer = optim.AdamW(params=self.parameters(), lr=self.cfg.learning_rate, weight_decay=self.cfg.weight_decay)
+        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=50, eta_min=1e-6)
         return [optimizer], [scheduler]
-    
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size = self.cfg.batch_size,
-                             shuffle = True, drop_last=True, num_workers=self.cfg.num_workers)
 
-    def val_dataloader(self):
-        return DataLoader(self.valid_dataset, batch_size = self.cfg.batch_size,
-                             shuffle = False, drop_last=False, num_workers=self.cfg.num_workers)
-    
-    def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size = self.cfg.batch_size,
-                             shuffle = False, drop_last=False, num_workers=self.cfg.num_workers)
-
-def visualize(preds, imgs, gts, num_imgs=10):
+def visualize(preds, imgs, gts):
     main_colors = torch.tensor([
         [0, 0, 0],
         [1, 0, 0],
@@ -179,55 +200,53 @@ def visualize(preds, imgs, gts, num_imgs=10):
     gts_onehot = F.one_hot(gts, num_classes=num_classes).permute(0, 3, 1, 2)
     gts_colored = torch.tensordot(gts_onehot.float(), colors, dims=[[1], [0]]).numpy()
 
-    fig, axs = plt.subplots(3, num_imgs, figsize=(9, 3))
-    fig.suptitle('Original --> Ground Truth --> Prediction')
+    num_imgs = min(len(preds), 10)
+    fig, axs = plt.subplots(3, num_imgs, figsize=(18, 6))
     for i in range(num_imgs):
-        img_num = np.random.randint(0, len(imgs))
-        axs[0, i].imshow(imgs[img_num], cmap='gray'); axs[0, i].axis('off') 
-        axs[1, i].imshow(gts_colored[img_num]); axs[1, i].axis('off')    
-        axs[2, i].imshow(preds_colored[img_num]); axs[2, i].axis('off')
+        axs[0, i].imshow(imgs[i], cmap='gray'); axs[0, i].axis('off') 
+        axs[1, i].imshow(gts_colored[i]); axs[1, i].axis('off')    
+        axs[2, i].imshow(preds_colored[i]); axs[2, i].axis('off')
+    fig.tight_layout()
     fig.show()
     return fig
 
 def main(cfg):
     # experiment tracker (you need to sign in with your account)
+    timestamp = time.time()
     wandb_logger = pl.loggers.WandbLogger(
-                            name='%s <- %d'%(cfg.exp_name, timestamp), 
+                            name='%s - %s <- %d'%(cfg.exp_name, cfg.comb_train_imgs, timestamp), 
                             group= '%s'%(cfg.exp_name), 
                             log_model=True, # save best model using checkpoint callback
-                            project='supervised-train',
-                            entity='ssl-medical-imaging',
-                            config=cfg,
-    )
+                            project=cfg.project_name, entity='ssl-medical-imaging', config=cfg)
 
-    # to save the best model on validation
+    # to save the best model on validation, log learning_rate and early stop
     checkpoint = pl.callbacks.ModelCheckpoint(
-        filename="best_model"+str(timestamp),
-        monitor="valid_loss",
-        save_top_k=1,
-        mode="max",
-        save_last=False,
-        save_weights_only=True,
-    )
+        filename="best_model_"+str(int(timestamp)),
+        monitor="valid_loss", save_top_k=1, save_last=False, mode="min")
     lr_monitor = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
+    early_stop = pl.callbacks.EarlyStopping(monitor="valid_loss", min_delta=0.00,
+                                            patience=cfg.patience, verbose=False, mode="min")
+
+    if cfg.checkpoint_path == 'random-init':
+      model = SegModel(cfg)
+    else:
+      model = SegModel.load_from_checkpoint(cfg=cfg,
+              checkpoint_path=cfg.checkpoint_path)
+      
+    # log gradients, parameter histogram and model topology
+    wandb_logger.watch(model, log='all')
 
     trainer = pl.Trainer(
         devices=cfg.num_gpus, accelerator="gpu", strategy="ddp",
-        logger=wandb_logger,
-        callbacks=[checkpoint, lr_monitor],
-        max_epochs=cfg.epochs,
-        precision=cfg.precision,
-    )
-
-    model = SegModel(cfg)
-    # log gradients, parameter histogram and model topology
-    wandb_logger.watch(model, log='all')
+        logger=wandb_logger, callbacks=[checkpoint, lr_monitor, early_stop],
+        max_epochs=cfg.epochs, precision=cfg.precision,
+        enable_progress_bar=cfg.enable_progress_bar)
     
     trainer.fit(model)
     print("------- Training Done! -------")
 
     print("------- Testing Begins! -------")
-    trainer.test(model)
+    trainer.test(model, ckpt_path=checkpoint.best_model_path)
 
 if __name__ == '__main__':
     main(cfg)
